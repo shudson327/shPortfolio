4b3d334c95a8a3dbb9b8fa88075199ae66a160c4
shudson327 <72040252+shudson327@users.noreply.github.com>
2020-11-18 22:59:17 -0500

    updated portfolio

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
     (function($) {
   "use strict"; // Start of use strict
 
